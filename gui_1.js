8c395b7e4de294dad75718b7a622477f5c04799b
Marwan Helali <marwan01@Marwans-MacBook-Pro.local>
2018-09-22 14:17:18 -0500

    can add candidates to table

--- gui_1.js
+++ gui_1.js
@@ -1,9 +1,58 @@
 var text
 var candidates = ["Ali", "Ahmed", "Omar"];
 
+
+$(document).ready( () => {
+    $('#addButton, #deleteButton').click(function () {
+        if (this.id == 'addButton') {
+
+            var candidateName = $("#inputBox").val();
+                if(candidateName !== "") {
+                addCandidate(candidateName);
+                $('#inputBox').val("");
+            }
+        }
+        else if (this.id == 'deleteButton') {
+            var candidateName = $("#inputBox").val();
+            deleteCandidate(candidateName);
+            $('#inputBox').val("");
+        }
+     });
+} );
+
+
+
 var container = $("inputBox");
 $("table").append("<tr><td>candiate one hardcoded yaay</td></tr>")
 
-for (var c in candidates) {
-    $("table").append("<tr><td>" +candidates[c]+ "</td></tr>")
+
+
+
+function performRotation(onElement)
+{
+	//alert("You clicked me!");
+	
+	onElement.css("transform", "rotate(90deg)");
+	
+
 }
+
+function addCandidate(candidate) {
+    candidates.push(candidate)
+        $("table").append("<tr><td>" +candidate+ "</td></tr>")
+        console.log("candidate "+ candidate + " has been added.")
+}
+
+function getUserInput() {
+    if($("#addButton").click())
+    var candidateName = $("#inputBox").val();
+    return candidateName
+}
+
+function save() {
+    
+    
+    $("#addButton").click();
+}
+
+
