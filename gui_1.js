4fbab0780e15e098af46456e31103944acbf160b
Marwan Helali <marwan01@Marwans-MacBook-Pro.local>
2018-09-22 14:54:33 -0500

    check for candidate duplication

--- gui_1.js
+++ gui_1.js
@@ -1,16 +1,12 @@
 var text
 var candidates = ["Ali", "Ahmed", "Omar"];
-
+var votes = [1, 3, 10]
 
 $(document).ready( () => {
     $('#addButton, #deleteButton').click(function () {
         if (this.id == 'addButton') {
-
             var candidateName = $("#inputBox").val();
-                if(candidateName !== "") {
-                addCandidate(candidateName);
-                $('#inputBox').val("");
-            }
+            addCandidate(candidateName);
         }
         else if (this.id == 'deleteButton') {
             var candidateName = $("#inputBox").val();
@@ -38,9 +34,55 @@ function performRotation(onElement)
 }
 
 function addCandidate(candidate) {
-    candidates.push(candidate)
-        $("table").append("<tr><td>" +candidate+ "</td></tr>")
-        console.log("candidate "+ candidate + " has been added.")
+
+    if(candidates.includes(candidate)) {
+        var userResponse = prompt(candidate + " already exists in the list. Would you like to reset the vote counts? enter y for yes or anything else to cancel");
+        if(userResponse === "y") {
+            votes[i] = 0;
+            // to work on later
+        }
+    }
+    else {
+        if(candidate !== "") {
+            candidates.push(candidate)
+            $("table").append("<tr><td>" +candidate+ "</td></tr>")
+            console.log("candidate: "+ candidate + " has been added.")
+            $('#inputBox').val("");
+        }          
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // var found = true;
+    // for(i = 0; i < candidates.length; i++ ) {
+    //     if(candidate == candidates[i]) {
+    //         found = true;
+    //         var userResponse = prompt(candidate + " already exists in the list. Would you like to reset the vote counts? enter y for yes or anything else to cancel");
+    //         if(userResponse === "y") {
+    //             votes[i] = 0;
+    //         }
+            
+    //     }
+    //     else {
+    //         found = false;
+    //     }
+    // }      
+    // if(candidate !== "" && !found) {
+    //     candidates.push(candidate)
+    //     $("table").append("<tr><td>" +candidate+ "</td></tr>")
+    //     console.log("candidate: "+ candidate + " has been added.")
+    //     $('#inputBox').val("");
+    // }          
 }
 
 function getUserInput() {
